35a43a40f340f01c3dfd41edc4f9ad43bc24aeb0
Chris Bram <cpbram2011@gmail.com>
2020-10-06 10:40:36 -0500

    Initialize MongoDb Seeder

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -4,8 +4,6 @@ import { Switch } from "react-router-dom";
 import NavBarContainer from "./nav/navbar_container";
 
 import MainPage from "./landing/landing-page";
-import LoginFormContainer from "./session/login_form_container";
-import SignupFormContainer from "./session/signup_form_container";
 import "../App.scss";
 import BackgroundVideo from "./landing/background-video";
 
@@ -15,7 +13,7 @@ const App = () => (
   <div className="app">
     <BackgroundVideo />
     <NavBarContainer />
-
+    <Modal />
     <AuthRoute exact path="/" component={MainPage} />
     {/* <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
