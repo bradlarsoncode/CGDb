07dbea4a04dd9c55991e5a5d6a3a97b68ddf0b49
Chris Bram <cpbram2011@gmail.com>
2020-10-06 14:57:11 -0500

    Heroku Ready III

--- app.jsx
+++ app.jsx
@@ -37,14 +37,7 @@ app.use(bodyParser.json());
 
 
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    })
-  }
 
-  
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
@@ -56,3 +49,11 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
