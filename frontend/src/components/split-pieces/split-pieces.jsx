58ef4707a1cafbf82783b8b6073b504d02d7ba79
Koexygen <rexarkoexygen@gmail.com>
2020-10-07 22:30:29 +0400

    split pieces integrated

---
+++ frontend/src/components/split-pieces/split-pieces.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import "./split-pieces.scss";
+
+const SplitPieces = () => {
+  return (
+    <div className="split-pieces-container">
+      <a
+        className="frame"
+        href="https://unsplash.com/photos/hEPmVFvF3Hs"
+        target="_blank"
+      >
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+      </a>
+    </div>
+  );
+};
+
+export default SplitPieces;
