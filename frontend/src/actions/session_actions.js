0b2691ba6f5deeb33380ea31332059661593b125
Koexygen <rexarkoexygen@gmail.com>
2020-10-07 23:48:49 +0400

    basics question and blood done

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -1,6 +1,5 @@
-
-import * as APIUtil from '../util/session_api_util';
-import jwt_decode from 'jwt-decode';
+import * as APIUtil from "../util/session_api_util";
+import jwt_decode from "jwt-decode";
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
@@ -8,48 +7,46 @@ export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 
 //TODO: remove redundant RECEIVE_USER_SIGN_IN
 
-export const receiveCurrentUser = currentUser => ({
-    type: RECEIVE_CURRENT_USER,
-    currentUser
+export const receiveCurrentUser = (currentUser) => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser,
 });
 
-
-export const receiveErrors = errors => ({
-    type: RECEIVE_SESSION_ERRORS,
-    errors
+export const receiveErrors = (errors) => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors,
 });
 
 export const logoutUser = () => ({
-    type: RECEIVE_USER_LOGOUT
+  type: RECEIVE_USER_LOGOUT,
 });
 
-export const signup = user => dispatch => {
-  debugger
-    return(
+export const signup = (user) => (dispatch) => {
+  return APIUtil.signup(user).then(
+    (res) => {
+      return dispatch(receiveCurrentUser(res.data));
+    },
+    (err) => {
+      return dispatch(receiveErrors(err.response.data));
+    }
+  );
+};
 
-    APIUtil.signup(user).then((res) => {
-        return dispatch(receiveCurrentUser(res.data)
-    )}, err => {
-        debugger
-        return dispatch(receiveErrors(err.response.data))
+export const login = (user) => (dispatch) =>
+  APIUtil.login(user)
+    .then((res) => {
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+      APIUtil.setAuthToken(token);
+      const decoded = jwt_decode(token);
+      dispatch(receiveCurrentUser(decoded));
     })
-)};
+    .catch((err) => {
+      dispatch(receiveErrors(err.response.data));
+    });
 
-export const login = user => dispatch => (
-    APIUtil.login(user).then(res => {
-        const { token } = res.data;
-        localStorage.setItem('jwtToken', token);
-        APIUtil.setAuthToken(token);
-        const decoded = jwt_decode(token);
-        dispatch(receiveCurrentUser(decoded))
-    })
-    .catch(err => {
-        dispatch(receiveErrors(err.response.data));
-    })
-)
-
-export const logout = () => dispatch => {
-    localStorage.removeItem('jwtToken')
-    APIUtil.setAuthToken(false)
-    dispatch(logoutUser())
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("jwtToken");
+  APIUtil.setAuthToken(false);
+  dispatch(logoutUser());
 };
