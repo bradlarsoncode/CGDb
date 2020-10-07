b2511c791983253360298f3610b7a972e6f397f1
Brad Larson <larsonbrad@pm.me>
2020-10-07 15:26:41 -0400

    added shadow text to assets

---
+++ frontend/src/assets/text.js
@@ -0,0 +1,16 @@
+//A long black shadow slid across the pavement near their feet. They were barely in time to see the huge gray form of the carnivore before it vanished behind a veil of smoke. 
+    //Can we add an effect where the 
+
+export const shadow = () => {
+    return(
+        <div className="fade-in">
+            A long black shadow slid across the pavement near their feet.
+            They were barely in time to see the huge gray form of the carnivore 
+            before it vanished 
+            behind 
+            a veil 
+            of smoke. 
+        </div>
+    ) 
+}
+
