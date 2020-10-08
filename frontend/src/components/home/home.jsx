298076130cf38659d990fd4779b25cf540f1a43d
dennis <dlum904@gmail.com>
2020-10-08 13:39:43 -0400

    gameover added

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -7,12 +7,11 @@ export default class Home extends React.Component {
     render () {
         if (this.props.currentUser.sanity < -12 ) {
             this.props.resetSanity(this.props.currentUser.email)
-            console.log(this.props.currentUser)
             return (
                 <div className ='gameover'>
                     <p> The CGDb thanks you for your participation today. Unfortunately, the medications aren’t working strong enough. Please remain still until a nurse can administer your medications. You know what happens if you try to move… See you tomorrow. </p>
+                    <p>- Drs. Chris, Gio, Dennis and Brad</p>
                 </div>
-
             )
         }
         else {
