258f9b8d9997bca95f69f5cf2b39d27f71543186
Koexygen <rexarkoexygen@gmail.com>
2020-10-09 00:05:07 +0400

    bug fix

--- frontend/src/components/question_show/question_show.jsx
+++ frontend/src/components/question_show/question_show.jsx
@@ -88,10 +88,9 @@ export default class QuestionShow extends React.Component {
           break;
       }
     return (
-      
       <div className="question-part">
-        {questionPart[this.props.progress]}
         <div className="q-form q-form-container">
+          {questionPart[this.props.progress]}
           <h3>{message}</h3>
           {ui}
           {this.props.questions ? (
@@ -122,8 +121,7 @@ export default class QuestionShow extends React.Component {
           ) : (
             <></>
           )}
-          <p>{this.state.errors}</p
-
+          <p>{this.state.errors}</p>
 
           <button onClick={this.handleSubmit}>Submit</button>
         </div>
