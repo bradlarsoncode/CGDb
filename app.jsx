4e06367fffc0c7e9001f2768087b9f51eb4b1507
Brad Larson <Larsonbrad@protonmail.com>
2020-10-23 11:22:04 -0400

    reverted back to old port implementation

--- app.jsx
+++ app.jsx
@@ -46,12 +46,10 @@ app.use("/api/users", users);
 app.use("/api/questions", questions);
 
 
-// const port = process.env.PORT || 5000;
-// app.listen(port, () => console.log(`Server is running on port ${port}`)); 
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`)); 
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+
 
 
 
