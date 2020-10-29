701a92e087ff485de50e68a2512d6f512cfd39ce
Chris Bram <cpbram2011@gmail.com>
2020-10-29 15:56:38 -0500

    That should be good

--- frontend/src/components/landing/landing-page.jsx
+++ frontend/src/components/landing/landing-page.jsx
@@ -1,9 +1,41 @@
 import React from "react";
 import "./landing-page.scss";
+import {connect} from 'react-redux';
+import {openModal} from '../../actions/modal_actions';
 
+const mSTP = state => ({
+  modal: state.modal
+})
+
+const mDTP = dispatch => ({
+  openModal: modal => dispatch(openModal(modal))
+})
 class MainPage extends React.Component {
   render() {
     return (
+      <div className="landing-parent ">
+        {!this.props.modal ? (
+        <div className="session-buttons">
+          <div className="btn-link">
+            <button
+              onClick={() => this.props.openModal("signup")}
+              className="link-inner"
+            >
+              Sign Up
+            </button>
+          </div>
+          <h3>or</h3>
+          <div className="btn-link">
+            <button
+              onClick={() => this.props.openModal("login")}
+              className="link-inner"
+            >
+              Log In
+            </button>
+          </div>
+        </div>
+        ) : (<></>)}
+
       <div className="landing-container ">
         <p>
           Founded in 1992 at Bram College, the goal of our database is to track
@@ -22,8 +54,9 @@ class MainPage extends React.Component {
         </p>
         <p>Note: This site is designed for adults, aged 18 or older.</p>
       </div>
+      </div>
     );
   }
 }
 
-export default MainPage;
+export default connect(mSTP, mDTP)(MainPage);
